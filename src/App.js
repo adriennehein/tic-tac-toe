5e606a62e5d90d2c829315bfb64a135b0abdada5
Adrienne Hein <adrienne.hein@gmail.com>
2018-10-04 11:27:10 -0700

    Adding project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header-bar">
+          X - Tic Tac Toe - O
         </header>
+
+        <div className="grid-board">
+
+          <div className="current-player">
+            its player Xs turn
+          </div>
+
+          <div className="game-grid">
+            <div className="grid-column">
+              <div className="grid-tile">
+              </div>
+              <div className="grid-tile">
+              </div>
+              <div className="grid-tile">
+              </div>
+            </div>
+            <div className="grid-column">
+              <div className="grid-tile">
+                <div className="x-piece">X</div>
+              </div>
+              <div className="grid-tile">
+              </div>
+              <div className="grid-tile">
+              </div>
+            </div>
+            <div className="grid-column">
+              <div className="grid-tile">
+              </div>
+              <div className="grid-tile">
+                <div className="o-piece">O</div>
+              </div>
+              <div className="grid-tile">
+              </div>
+            </div>
+          </div>
+
+          <div className="new-game">
+            new game
+          </div>
+        </div>
+
+        <footer className="footer-bar">
+          Built with React by Adrienne Hein 2018
+        </footer>
+
       </div>
     );
   }
